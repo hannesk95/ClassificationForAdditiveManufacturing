de782a27167c04c7fbf036986e6d310ecbe3b49d
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-04-22 22:11:17 +0200

    update Docker conatainer

--- infra/entrypoint.sh
+++ infra/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 # This is the command to be executed when the container starts
 
-python3 example.py
+# Clone Project Repository
+rm -rf TUM-DI-LAB
+git clone https://hannesk95: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@github.com/semester-project/TUM-DI-LAB.git
+
+# Clone Kaolin framework
+rm -rf kaolin
+git clone --recursive https://github.com/NVIDIAGameWorks/kaolin
+#cd /kaolin && git checkout v0.9.0
+#cd /kaolin && python3 setup.py develop
+
+# Clone PyMesh framework
+rm -rf PyMesh
+git clone https://github.com/PyMesh/PyMesh.git
+#cd PyMesh && git submodule update --init && export PYMESH_PATH=`pwd`
+/bin/bash
--- infra/run.sh
+++ infra/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Build docker container image
-docker build -t "di-lab" .
+docker build -t tum-di-lab .
 
 # Run container image
-docker run --rm --hostname DI-LAB di-lab
+docker run -it --rm tum-di-lab
