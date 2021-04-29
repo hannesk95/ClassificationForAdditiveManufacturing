67f803dbe369ee0dfab4417cb1fbaa549b2ed0e3
fxbk <44337498+fxbk@users.noreply.github.com>
2021-04-29 20:12:02 +0200

    Updated dataset class and added __getitem__ method

--- src/dataset.py
+++ src/dataset.py
@@ -1,15 +1,25 @@
 import os
+from stl import mesh
 
 
 class Dataset:
-    def __init__(self, dir, transform=None, limit_files=None):
+    def __init__(self, dir_path, transform=None, limit_files=None):
         self.limit_files = limit_files
-        self.models = os.listdir(dir)
+        self.models = os.listdir(dir_path)
         self.transform = transform
         
     def __len__(self):
         return len(self.models)
 
+    def __getitem__(self, index):
+        model_path = self.models[index]
+        model = mesh.Mesh.from_file(model_path)
+
+        if self.transform is not None:
+            model = self.transform(model)
+
+        # Save model
+        
 
 class ComposeTransform:
     """Transform class that combines multiple other transforms into one"""
