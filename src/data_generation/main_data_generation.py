f3c92813f08b719463e18bd85dba0f559bdc6ae8
fxbk <44337498+fxbk@users.noreply.github.com>
2021-04-30 21:32:22 +0200

    Extended main_data_generation with test run for dataset class

--- src/data_generation/main_data_generation.py
+++ src/data_generation/main_data_generation.py
@@ -1,16 +1,21 @@
 import logging
+import os
 from transformations import Voxelizer, Normalizer, ComposeTransformer
 from dataset import Dataset
+from data_preprocessor import DataPreprocessor
 
 
 def main():
+    model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data'))
+    dataset = Dataset(model_path)
+    dataset[0]
     pass
 
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', level=logging.DEBUG)
-    logging.info('Started main')
+    logging.info('Started main_data_generation')
 
     main()
 
-    logging.info('Finished main')
+    logging.info('Finished main_data_generation')
