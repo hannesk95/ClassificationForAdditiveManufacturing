7900574bf8db9ba1089c81ff0418817753870749
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-05-01 13:22:56 +0200

    finalize dataset fetching script

--- data/ABC_dataset/fetch_ABC_dataset.py
+++ data/ABC_dataset/fetch_ABC_dataset.py
@@ -1,11 +1,65 @@
+import os
 import subprocess
+from tqdm import tqdm
+from shutil import copyfile, rmtree
+from pyunpack import Archive
+
+def start_download():
+    """
+    This function creates a directory, if it does not exist already, in order to store the
+    .stl files and starts the download.
+    """
+
+    directory = "stl_files"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    os.chdir('./utils/')
+    subprocess.call(['sh', './download.sh'])
 
 
-#cat stl2_v00.txt | xargs -n 2 -P 8 sh -c 'wget --no-check-certificate $0 -O stl/$1'
-#cat stl2_v00.txt | xargs -n 2 -P 8 sh -c 'wget --no-check-certificate $0 -O stl2/$1'
+def unzip_directory():
+    """
+    This function takes the 7z archive and extracts it.
+    """
+    zip_name = os.listdir("./stl_files")
+    zip_path = "./stl_files" + "/" + zip_name[0]
 
-command = "cat" + " stl2_v00.txt" + " |" + " xargs" + " -n" + " 2" + " -P" + " 8" + " sh" " -c" + " 'wget --no-check-certificate $0 -O stl2/$1'"
+    Archive(zip_path).extractall("./stl_files")
+    rmtree(zip_path, ignore_errors=True)
 
-process = subprocess.Popen(command)
+
+def flatten_directory():
+    """
+    This function copies every .stl file which is stored in a sub-folder into its
+    parent directory.
+    """
+
+    cwd = os.getcwd()
+    dataset_path = cwd + "\\" + "stl_files"
+
+    os.chdir(dataset_path)
+    folders = os.listdir()
+
+    for i in tqdm(range(len(folders))):
+
+        if folders[i].endswith("7z"):
+            continue
+
+        os.chdir(dataset_path + "\\" + folders[i])
+        filename = os.listdir()
+        src = dataset_path + "\\" + folders[i] + "\\" + filename[0]
+        dst = dataset_path + "\\" + filename[0]
+        copyfile(src, dst)
+        os.chdir(dataset_path)
+        rmtree(dataset_path + "\\" + folders[i], ignore_errors=True)
 
 
+if __name__ == "__main__":
+    start_download()
+    unzip_directory()
+    flatten_directory()
+
+    print("###############################################################################")
+    print("[SUCCESS]: Download and processing of the specified ABC data chunk is complete!")
+    print("###############################################################################")
