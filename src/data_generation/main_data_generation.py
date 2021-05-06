f5fccf49fbc9a0f2fd5d0c12d2a38f739bb902fc
fxbk <44337498+fxbk@users.noreply.github.com>
2021-05-06 16:34:49 +0200

    Together with Hannes sketched out the data generation pipeline

--- src/data_generation/main_data_generation.py
+++ src/data_generation/main_data_generation.py
@@ -1,38 +1,38 @@
 import logging
 import os
-from transformations import Voxelizer, Normalizer, ComposeTransformer
+from transformations import Normalizer, Aligner, Voxelizer, Defector, ComposeTransformer
 from dataset import Dataset
 from data_preprocessor import DataPreprocessor
 
-from data_generation import BatchDataLoader, ModelSelector
+from data_generation import BatchDataProcessor, ModelSelector
 
 
 def main():
-
     # Location of our dataset
+    # TODO create config and read config here
     model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data'))
+    target_path =
+    max_file_number =
+    max_filesize =
+    min_compactness =
+    batch_size =
 
     # 1. Preselect files
     model_selector = ModelSelector(input_path=model_path, max_filesize=1, min_compactness=None)
     final_models = model_selector()
 
-    # 2. Start loading data in batches
-    loader = BatchDataLoader(filepaths=final_models, batch_size=500)
+    # 2. Define transformations
+    normalizer = Normalizer()
+    aligner = Aligner()
+    voxelizer = Voxelizer()
+    defector = Defector()
 
-    for model in loader():
-        pass
+    # 3. Compose transformations
+    composer = ComposeTransformer([normalizer, aligner, voxelizer, defector])
 
-        # 3. Align model
-
-        # 4. Normalize model
-
-        # 5. Voxelize model + save model + save label "printable)
-
-        # 6. Add defect to model + save model + save label "not printable"
-
-    dataset = Dataset(model_path)
-    dataset[0]
-    pass
+    # 4. Start loading data in batches
+    loader = BatchDataProcessor(final_models, batch_size=500, transform=composer)
+    loader()
 
 
 if __name__ == '__main__':
