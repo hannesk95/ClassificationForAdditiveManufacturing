e603c6d6b266b960cc361d653bc607cb731e12e2
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-05-06 09:47:53 +0200

    update main_data_generation.py

--- src/data_generation/main_data_generation.py
+++ src/data_generation/main_data_generation.py
@@ -4,9 +4,32 @@ from transformations import Voxelizer, Normalizer, ComposeTransformer
 from dataset import Dataset
 from data_preprocessor import DataPreprocessor
 
+from data_generation import BatchDataLoader, ModelSelector
+
 
 def main():
+
+    # Location of our dataset
     model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', 'data'))
+
+    # 1. Preselect files
+    model_selector = ModelSelector(input_path=model_path, max_filesize=1, min_compactness=None)
+    final_models = model_selector()
+
+    # 2. Start loading data in batches
+    loader = BatchDataLoader(filepaths=final_models, batch_size=500)
+
+    for model in loader():
+        pass
+
+        # 3. Aligne model
+
+        # 4. Normalize model
+
+        # 5. Voxelize model + save model + save label "printable)
+
+        # 6. Add defect to model + save model + save label "not printable"
+
     dataset = Dataset(model_path)
     dataset[0]
     pass
