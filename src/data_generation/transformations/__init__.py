6351f00f4fefe0d25fe166fc63615788d9c8d02a
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-05-06 19:16:59 +0200

    update __init__ according to name refactoring

--- src/data_generation/transformations/__init__.py
+++ src/data_generation/transformations/__init__.py
@@ -2,4 +2,4 @@ from .Voxelizer import Voxelizer
 from .Normalizer import Normalizer
 from .Aligner import Aligner
 from .Defector import Defector
-from .composetransform import ComposeTransformer
+from .ComposeTransformer import ComposeTransformer
