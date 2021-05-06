1b7c5768a31d2a4c7f7c6a4067a58a3ef7a8f54d
fxbk <44337498+fxbk@users.noreply.github.com>
2021-05-06 16:32:00 +0200

    Added functionality from MeshReader and MeshSaver to model class

---
+++ src/data_generation/model.py
@@ -0,0 +1,60 @@
+import os
+import logging
+import numpy as np
+import open3d as o3d
+
+from src.data_generation.utils import extract_file_name
+
+
+class Model:
+    """
+    Model class, for handling mesh and voxel models
+
+    :param path: path to the .obj/.stl file to be read
+    """
+    def __init__(self, path):
+        self.path = path
+        self.mesh, self.vertices, self.normals, self.faces = self.load_model(path)
+        self.label = 1
+        self.model_name = extract_file_name(path)
+        self.voxel_rep = None
+
+    def load_model(self):
+        mesh = o3d.io.read_triangle_mesh(self.path)
+        mesh.compute_vertex_normals()
+        return mesh, np.asarray(mesh.vertices), mesh.triangle_normals, np.asarray(mesh.triangles)
+
+    def save_as_mesh(self, target_path=None):
+        if target_path is None:
+            target_path = self.path
+
+        # from numpy array to o3d Vector3dVector
+        vertices_o3d = o3d.utility.Vector3dVector(self.vertices)
+        # from numpy array to o3d Vector3iVector
+        faces_o3d = o3d.utility.Vector3iVector(self.faces)
+        # create o3d TriangleMesh
+        mesh = o3d.geometry.TriangleMesh(vertices=vertices_o3d, triangles=faces_o3d)
+        # compute normal to save as stl file
+        mesh.compute_triangle_normals()
+        # save the mesh to path
+        o3d.io.write_triangle_mesh(target_path, mesh)
+
+    def save_as_npz(self, target_path):
+        """
+        Saves the voxelized model as compressed npz array + the label
+        :param target_path: target_path where to store the model
+        :return: -
+        """
+        if self.model_name is None:
+            logging.ERROR('Model was not voxelized. Please voxelize the model before saving it')
+        else:
+            np.savez_compressed(os.path.join(target_path, self.model_name), model=self.voxel_rep, label=self.label)
+
+    def get_model_data(self):
+        """Returns vertices, normals and faces"""
+        return self.vertices, self.normals, self.faces
+
+    def set_model_data(self, vertices, normals, faces):
+        self.vertices = vertices
+        self.normals = normals
+        self.faces = faces
