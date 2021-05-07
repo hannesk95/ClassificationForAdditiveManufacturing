dc3fe3fc47c800d79ce8de47fcb7924eafa1322e
Ahmed Ebid <ahmedhamdi96@live.com>
2021-05-07 04:26:34 +0200

    added mesh checks + vertices/edges/triangles fixes

--- src/DataCleaner.py
+++ src/DataCleaner.py
@@ -6,12 +6,11 @@ from MeshReader import MeshReader
 from MeshSaver import MeshSaver
 
 
-#####Clean the model using pymeshlab
-
 class DataCleaner():
     def __init__(self,arg):
         self.arg = arg
 
+    # Clean the model using pymeshlab
     def clean(self, vertices, faces):
         """
         Creates
@@ -36,6 +35,54 @@ class DataCleaner():
         faces_cleaned =  np.array(face_matrix)
         vertices_cleaned =  np.array(vertex_matrix)
         return vertices_cleaned,faces_cleaned
+
+    def mesh_checks(self, mesh):
+        """
+        Prints O3D TriangleMesh validity checks
+
+        :param mesh: O3D mesh object
+        """
+        # tests if all vertices are manifold
+        print("all vertices manifold:", mesh.is_vertex_manifold())
+        # tests if all edges are manifold
+        print("all edges manifold:", mesh.is_edge_manifold())
+        # tests if the mesh is watertight
+        print("mesh is watertight:", mesh.is_watertight())
+
+    def fix_mesh_vertices(self, mesh):
+        """
+        Applies O3D TriangleMesh vertices fixes
+
+        :param mesh: O3D mesh object
+        """
+        # removes vertices that have identical coordinates
+        mesh.remove_duplicated_vertices()
+        # removes vertices that are not referenced in any triangle
+        mesh.remove_unreferenced_vertices()
+
+    def fix_mesh_edges(self, mesh):
+        """
+        Applies O3D TriangleMesh edges fixes
+
+        :param mesh: O3D mesh object
+        """
+        # removes all non-manifold edges
+        # by successively deleting triangles with the smallest surface area 
+        # adjacent to the non-manifold edge until the number of adjacent 
+        # triangles to the edge is <= 2
+        mesh.remove_non_manifold_edges()
+
+    def fix_mesh_triangles(self, mesh):
+        """
+        Applies O3D TriangleMesh triangles fixes
+
+        :param mesh: O3D mesh object
+        """
+        # removes triangles that reference the same three vertices
+        mesh.remove_duplicated_triangles()
+        # removes triangles that reference a single vertex multiple times in a single triangle
+        # usually happens as a result of removing duplicated vertices
+        mesh.remove_degenerate_triangles()
 
 ##test
 
