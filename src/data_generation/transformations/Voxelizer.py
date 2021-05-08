70f389829c4a3acea1cc04d7b28ca41f74a2ba26
fxbk <44337498+fxbk@users.noreply.github.com>
2021-05-08 21:54:17 +0200

    Refactored Voxlizer. i.e. added __call__ method, made use of the model class, removed unecessary code

---
+++ src/data_generation/transformations/Voxelizer.py
@@ -0,0 +1,54 @@
+from mesh_to_sdf import mesh_to_voxels
+import numpy as np
+import trimesh
+from skimage import measure
+
+
+def sdf_to_binary(sdf):
+    """
+    The function takes in sdf values and converts it into a binary field.
+    : sdf: N x N x N array of sdf values where N is the number of voxels.
+
+    """
+    binay_voxels = np.ones_like(sdf)
+    dims = [sdf.shape[0], sdf.shape[1], sdf.shape[2]]
+
+    for i in range(dims[0]):
+        for j in range(dims[1]):
+            for k in range(dims[2]):
+                if abs(float(sdf[i][j][k])) > 0.05:
+                    binay_voxels[i][j][k] = 0
+    return binay_voxels
+
+
+def visualise_sdf(sdf):
+    """
+    The function reconstructs the mesh using marching cubes algorithm and renders it.
+    : sdf: N x N x N array of sdf values where N is the number of voxels.
+    """
+    vertices, faces, normals, _ = measure.marching_cubes(sdf, level=0)
+    mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_normals=normals)
+    mesh.show()
+
+
+class Voxelizer:
+    def __init__(self, dimension=64, representation='occupancy'):
+        self.dimension = dimension
+        self.representation = representation
+
+    def __call__(self, model):
+        voxels = self.mesh_to_sdf(model)
+        if self.representation == 'sdf':
+            model.set_voxel_rep(voxels)
+        else:
+            model.set_voxel_rep(sdf_to_binary(voxels))
+        return model
+
+    def mesh_to_sdf(self, model):
+        """
+        The function converts an input triangular mesh into a voxelized array.
+        """
+        vertices, normals, faces = model.get_model_data()
+        mesh = trimesh.Trimesh(vertices=vertices, faces=faces, vertex_normals=normals)
+        voxels = mesh_to_voxels(mesh, self.dimension, sign_method='depth', pad=False)
+        return voxels
