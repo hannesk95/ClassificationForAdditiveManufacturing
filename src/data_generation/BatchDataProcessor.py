d73314dac602de4943b10231833b97db57969700
fxbk <44337498+fxbk@users.noreply.github.com>
2021-05-13 12:42:56 +0200

    Fix: _load_data_batch method to load multiple batches
    The previoies version only loaded one batch and then returned empty lists. I therefore add the the additional for loop over the batches and one last iteration over the rest of the models, @hannesk95 maybe you can have a look at it as well

--- src/data_generation/BatchDataProcessor.py
+++ src/data_generation/BatchDataProcessor.py
@@ -1,4 +1,5 @@
 from tqdm import tqdm
+import numpy as np
 from src.data_generation.MeshModel import MeshModel
 
 
@@ -11,22 +12,31 @@ class BatchDataProcessor:
         self.filepaths = filepaths
         self.file = None
         self.batch_size = batch_size
-        self.data_batch = []
         self.pointer = 0
         self.transformer = transformer
         self.target_path = target_path
 
     def _load_data_batch(self):
-        for self.file in tqdm(self.filepaths[self.pointer:(self.pointer + self.batch_size)],
-                              desc=f"[INFO]: Loading data batches of size {self.batch_size}!"):
-            self.data_batch.append(MeshModel(self.file))
+        for _ in tqdm(range(int(np.floor(len(self.filepaths) / self.batch_size))), desc="[INFO]: Processing batch"):
+            data_batch = []
+            for self.file in tqdm(self.filepaths[self.pointer:(self.pointer + self.batch_size)],
+                                  desc=f"[INFO]:    Loading data batches of size {self.batch_size}!"):
+                data_batch.append(MeshModel(self.file))
             self.pointer += self.batch_size
 
-        yield self.data_batch
+            yield data_batch
+
+        data_batch = []
+        if self.pointer != len(self.filepaths):
+            for self.file in tqdm(self.filepaths[self.pointer:len(self.filepaths)],
+                                  desc=f"[INFO]:    Loading data batches of size {len(self.filepaths) - self.pointer}!"):
+                data_batch.append(MeshModel(self.file))
+
+            yield data_batch
 
     def process(self):
         for batch in self._load_data_batch():
-            for model in tqdm(batch, desc="[INFO]: Running models through the pipeline"):
+            for model in tqdm(batch, desc="[INFO]:    Running models through the pipeline"):
                 if self.transformer is not None:
                     model = self.transformer(model)
                 model.save(self.target_path)
