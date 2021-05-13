5c4977120fc2872e58a2800adbf233600479795c
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-05-13 17:50:29 +0200

    refactoring of status output

--- src/data_generation/BatchDataProcessor.py
+++ src/data_generation/BatchDataProcessor.py
@@ -20,7 +20,7 @@ class BatchDataProcessor:
         for _ in tqdm(range(int(np.floor(len(self.filepaths) / self.batch_size))), desc="[INFO]: Processing batch"):
             data_batch = []
             for self.file in tqdm(self.filepaths[self.pointer:(self.pointer + self.batch_size)],
-                                  desc=f"[INFO]:    Loading data batches of size {self.batch_size}!"):
+                                  desc=f"[INFO]: Loading data batches of size {self.batch_size}!"):
                 data_batch.append(MeshModel(self.file))
             self.pointer += self.batch_size
 
@@ -29,14 +29,14 @@ class BatchDataProcessor:
         data_batch = []
         if self.pointer != len(self.filepaths):
             for self.file in tqdm(self.filepaths[self.pointer:len(self.filepaths)],
-                                  desc=f"[INFO]:    Loading data batches of size {len(self.filepaths) - self.pointer}!"):
+                                  desc=f"[INFO]: Loading data batches of size {len(self.filepaths) - self.pointer}!"):
                 data_batch.append(MeshModel(self.file))
 
             yield data_batch
 
     def process(self):
         for batch in self._load_data_batch():
-            for model in tqdm(batch, desc="[INFO]:    Running models through the pipeline"):
+            for model in tqdm(batch, desc="[INFO]: Running models through the pipeline"):
                 if self.transformer is not None:
                     model = self.transformer(model)
                 model.save(self.target_path)
