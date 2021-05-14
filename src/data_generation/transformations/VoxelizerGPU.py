9640765cd95d4a48698f054cfe4dc7b1762118d3
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-05-14 02:42:34 +0200

    finalize GPU Voxelizer

--- src/data_generation/transformations/VoxelizerGPU.py
+++ src/data_generation/transformations/VoxelizerGPU.py
@@ -3,7 +3,6 @@ import subprocess
 import open3d as o3d
 from pathlib import Path
 from src.data_generation.utils import binvox2npz
-from src.data_generation.utils import extract_file_name
 
 
 class VoxelizerGPU:
@@ -12,25 +11,38 @@ class VoxelizerGPU:
         self.dimension = dimension
 
     def __call__(self, model: object):
+        """ Call method which starts the CUDA voxelizer. Depending on the input file format,
+        the input file is either converted first into .obj file format, followed by a blocking SHELL
+        command execution or done straight forward. Finally, the resulting .binvox file is converted
+        into a numpy array and stored in a compressed file format."""
 
         model_path = model.path
 
         if model.path.endswith(".stl"):
-            model_path = self._convert_stl2obj(model)
+            model_path = self._convert_stl2obj(model_path)
+            cmd = self._get_shell_command(model_path)
+            subprocess.call(cmd, shell=True)
+            binvox2npz(Path(model_path).with_suffix('.binvox'))
+            os.remove(model_path)
 
-        cmd = self._get_shell_command(model_path)
-        subprocess.call(cmd)
-        binvox2npz(Path(model_path).with_suffix('.binvox'))
-        os.remove(model_path)
+        else:
+            cmd = self._get_shell_command(model_path)
+            subprocess.call(cmd, shell=True)
+            binvox2npz(Path(model_path).with_suffix('.binvox'))
 
-    def _convert_stl2obj(self, model: object) -> str:
-        mesh_stl = o3d.io.read_triangle_mesh(model.path)
-        mesh_obj_path = Path(model.path).with_suffix('.obj')
+    def _convert_stl2obj(self, model_path: str) -> str:
+        """ Internal method in order to convert .stl files into .obj files.
+        This is needed as the CUDA voxelizer requires .obj input file format. """
+
+        mesh_stl = o3d.io.read_triangle_mesh(model_path)
+        mesh_obj_path = Path(model_path).with_suffix('.obj')
         o3d.io.write_triangle_mesh(mesh_obj_path, mesh_stl)
 
         return mesh_obj_path
 
     def _get_shell_command(self, model_path: str) -> str:
+        """ Internal method in order to prepare the shell command to be executed in order
+        to start the CUDA voxelizer. """
 
         path_voxelizer = "./build/bin/voxelizer "
         resolution = f"-r {self.dimension} "
@@ -38,5 +50,3 @@ class VoxelizerGPU:
         path_output = os.path.join(Path(model_path).with_suffix(''), "_voxelized_", str(self.dimension))
 
         return os.path.join(path_voxelizer, resolution, path_input, path_output)
-
-        # "./voxelizer -r 64 ./data/sphere/sphere.obj ./data/sphere/sphere_voxelized_64"
