bd2b21158f2e6ce9381c1b8c3ab101df828e139c
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-05-20 19:26:51 +0200

    apply PEP8 refactoring

--- src/deep_learning/main_deep_learning.py
+++ src/deep_learning/main_deep_learning.py
@@ -3,7 +3,11 @@ import train
 import wandb
 import configuration
 
-params = dict(epochs=configuration.training_configuration.number_epochs,batch_size=configuration.training_configuration.batch_size,lr=configuration.training_configuration.learning_rate,dataset_size=configuration.train_data_configuration.training_data_size,resnet_depth=configuration.training_configuration.resnet_depth)
+params = dict(epochs=configuration.training_configuration.number_epochs,
+              batch_size=configuration.training_configuration.batch_size,
+              lr=configuration.training_configuration.learning_rate,
+              dataset_size=configuration.train_data_configuration.training_data_size,
+              resnet_depth=configuration.training_configuration.resnet_depth)
 
 
 def main():
