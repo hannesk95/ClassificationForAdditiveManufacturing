4047dd0335741df17b5aaefb5897d16b77be0056
fxbk <44337498+fxbk@users.noreply.github.com>
2021-05-24 11:15:04 +0200

    Feat: check_hole_feasibility to check if a give hole is feasible
    I.e. checks if the hole are completly through the model

--- src/data_generation/transformations/DefectorRotation.py
+++ src/data_generation/transformations/DefectorRotation.py
@@ -1,3 +1,4 @@
+import logging
 import numpy as np
 import random
 from scipy.ndimage.interpolation import rotate
@@ -10,10 +11,10 @@ def add_vertical_hole(model_data, radius, offset):
     xx = np.arange(model_data.shape[0])
     yy = np.arange(model_data.shape[1])
     out = np.zeros_like(model_data)
+    inside = (xx[:, None] - offset[0]) ** 2 + (yy[None, :] - offset[1]) ** 2 > (radius ** 2)
 
     for idz in range(model_data.shape[2]):
         voxel_sim_defect_circle_cut = model_data[:, :, idz].astype(np.int32)
-        inside = (xx[:, None] - offset[0]) ** 2 + (yy[None, :] - offset[1]) ** 2 > (radius ** 2)
         out[:, :, idz] = (voxel_sim_defect_circle_cut & inside)
 
     model_data_with_defect = np.array(out)
@@ -60,12 +61,25 @@ def determine_last_unique_horizontal_elements(source, number_of_elements):
     return to_remove
 
 
+def check_hole_feasibility(model_data, radius, offset):
+    xx = np.arange(model_data.shape[0])
+    yy = np.arange(model_data.shape[1])
+    top_down_view = np.sum(model_data, axis=2)
+    inside = (xx[:, None] - offset[0]) ** 2 + (yy[None, :] - offset[1]) ** 2 > (radius ** 2)
+    inidices_to_remove = np.array(np.where(inside == False)).T
+    for indices in inidices_to_remove:
+        if top_down_view[indices[0], indices[1]] == 0:
+            return False
+    return True
+
+
 class DefectorRotation:
-    def __init(self, radius=2, border=5, rotation=True, visualize_top_down_view=False):
+    def __init__(self, radius=2, border=5, rotation=True, visualize_top_down_view=False, number_of_trials=5):
         self.radius = radius
         self.border = border
         self.rotation = rotation
         self.visualize_top_down_view = visualize_top_down_view
+        self.number_of_trials = number_of_trials
 
     def __call__(self, model):
         model_data = model.model
@@ -97,23 +111,35 @@ class DefectorRotation:
         # Remove elements at the border
         possible_offsets_final = np.delete(possible_offsets, list(set(to_remove)), axis=0)
 
-        offset = possible_offsets_final[random.randrange(0, len(possible_offsets_final))]
-        offset.astype(int)
+        for trial in range(self.number_of_trials):
+            offset = possible_offsets_final[random.randrange(0, len(possible_offsets_final))]
+            offset.astype(int)
+            if check_hole_feasibility(model_data, self.radius, offset):
+                break
+
+        if (trial-1) == self.number_of_trials:
+            logging.warning(f'Could not find feasible offset for model: {model.model_name}')
+            return model
+
         model_data = add_vertical_hole(model.model, self.radius, offset)
 
         if self.rotation:
             # Rotate model back
-            model_data = np.around(rotate(model_data, 360-x_rotation))
-            model_data = np.around(rotate(model_data, 360-y_rotation, (1, 2)))
-            model_data = np.around(rotate(model_data, 360-z_rotation, (0, 2)))
+            model_data = np.around(rotate(model_data, 360 - x_rotation))
+            model_data = np.around(rotate(model_data, 360 - y_rotation, (1, 2)))
+            model_data = np.around(rotate(model_data, 360 - z_rotation, (0, 2)))
 
         # TODO Put model in shape as before
-        # TODO Add more checks
-        # TODO Find way on how to save model with defect (first idea return list of models
-        
+        # TODO Find way on how to save model with defect (first idea return list of models)
+
         if self.visualize_top_down_view:
+            basis = np.zeros_like(top_down_view)
+            for indices in possible_offsets_final:
+                idx = indices[0]
+                idy = indices[1]
+                basis[idx, idy] = 1
             top_down_view = np.sum(model_data, axis=2)
-            sns.heatmap(top_down_view)
+            sns.heatmap(basis + (top_down_view > 0))
 
         model_with_defect = VoxelModel(model_data, np.array([0]), model.model_name + f'_defect_radius{self.radius}')
 
