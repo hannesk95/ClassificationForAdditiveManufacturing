3bd35f4ad1e601f82fbf8ea0d76a120808497909
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-05-25 22:34:58 +0200

    checkout dev branch after cloning repository, copy config.ini file into repository

--- infra/data_generation_container/entrypoint.sh
+++ infra/data_generation_container/entrypoint.sh
@@ -2,6 +2,12 @@
 
 # Clone current status of project repository
 git clone https://hannesk95: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@github.com/semester-project/TUM-DI-LAB.git
+cd TUM-DI-LAB
+git checkout dev
+cd /
+
+# Copy config.ini file into repository directory
+mv config.ini TUM-DI-LAB/src/data_generation/config.ini
 
 # Clone and compile binaries for CUDA voxelizer
 git clone https://github.com/kctess5/voxelizer.git
