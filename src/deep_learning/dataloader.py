0b180fb49d475a99202c4543548745d051e4b438
ge46tik <aditya.srinivas@tum.de>
2021-05-29 22:25:11 +0200

    Implementation of dataloader

--- src/deep_learning/dataloader.py
+++ src/deep_learning/dataloader.py
@@ -1,3 +1,4 @@
+from numpy.lib.utils import source
 import torch
 import os
 import random
@@ -9,16 +10,46 @@ class Data(Dataset):
     def __init__(self,transform=None):
         super(Data,self).__init__()
         self.transform = transform
+        self.model_data = self.load_model_path()
 
     def __len__(self):
         return configuration.train_data_configuration.training_data_size
 
+    def load_model_path(self):
+        source_dir = configuration.train_data_configuration.training_set_dir
+        path, dirs, files = next(os.walk(source_dir))
+        files = sorted(files)
+
+        models = self.model_initiliazer()
+
+        for i in range(len(files)):
+            data = np.load(source_dir + files[i])['model']
+            label = np.load(source_dir + files[i])['label']
+            models[i].append((data,label))
+
+        print("Data loading complete")
+        return models
+
+    def model_initiliazer(self):
+        model_data = {}
+        source_dir = configuration.train_data_configuration.training_set_dir
+        path, dirs, files = next(os.walk(source_dir))
+        files = sorted(files)
+
+        for i in range(len(files)):
+            model_data[i] = []
+        return model_data
+
     def __getitem__(self, index):
-        model = np.load("")['model']
-        label = np.load("")['label']
+        source_dir = configuration.train_data_configuration.training_set_dir
+        path, dirs, files = next(os.walk(source_dir))
+        files = sorted(files)
+
+        id = random.randint(0,len(files))
+        data, label = random.choice(self.model_data[id])
 
         if self.transform is not None:
-            model = torch.Tensor([model])
+            data = torch.Tensor([data])
         
-        return model,label
+        return data,label
         
