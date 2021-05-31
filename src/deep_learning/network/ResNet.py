13dc60a36ae4aba88155b46ce1cc2f47f7bc027c
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-05-31 18:18:24 +0200

    buf fix main function for testing purposes

--- src/deep_learning/network/ResNet.py
+++ src/deep_learning/network/ResNet.py
@@ -153,12 +153,11 @@ def generate_model(model_depth, **kwargs):
     return model
 
 
-#For test only
+# For test only
 if __name__ == '__main__':
     net = generate_model(101)
     
     if torch.cuda.device_count() > 0:
-        summary(net.cuda(),(1,64,64,64))_
+        summary(net.cuda(),(1,64,64,64))
     else:
         summary(net,(1,64,64,64))
-    
