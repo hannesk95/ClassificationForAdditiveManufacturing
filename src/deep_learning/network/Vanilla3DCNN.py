952f2afcb3ddfd98e6061bca1dc4986b63d42fb6
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-01 09:54:25 +0200

    update vanilla 3D CNN

--- src/deep_learning/network/Vanilla3DCNN.py
+++ src/deep_learning/network/Vanilla3DCNN.py
@@ -1,36 +1,53 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torchsummary import summary
 
 
 class Vanilla3DCNN(nn.Module):
-    def __init__(self, num_classes):
-        super(Vanilla3DCNN, self).__init__()
-        self.conv1 = nn.Conv3d(1, 32, kernel_size=(9, 9, 9))
-        self.conv2 = nn.Conv3d(32, 32, kernel_size=(7, 7, 7))
-        self.conv3 = nn.Conv3d(32, 64, kernel_size=(5, 5, 5))
-        self.fc1 = nn.Linear(3*3*64, 256)
-        self.fc2 = nn.Linear(256, num_classes)
+    """#TODO: Add docstring."""
 
-        self.dropout = nn.Dropout(p=0.1)
-        self.dropout3d = nn.Dropout3d(p=0.1)
+    def __init__(self, num_classes):
+        """#TODO: Add docstring."""
+
+        super(Vanilla3DCNN, self).__init__()
+
+        self.conv1 = nn.Conv3d(in_channels=1, out_channels=32, kernel_size=(9, 9, 9))
+        self.conv2 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=(7, 7, 7))
+        self.conv3 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(5, 5, 5))
+        self.fc1 = nn.Linear(64, 64)
+        self.fc2 = nn.Linear(64, num_classes)
+
         self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.2)
+        self.dropout3d = nn.Dropout3d(p=0.2)
+        self.max_pool3d = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=2)
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.dropout(x, p=0.2)
-        x = F.relu(F.max_pool3d(self.conv2(x), 2))
-        x = F.dropout(x, p=0.2)
-        x = F.relu(F.max_pool3d(self.conv3(x),2))
-        x = F.dropout(x, p=0.2)
-        x = x.view(-1, 3*3*64 )
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
+        """#TODO: Add docstring."""
+
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.dropout3d(x)
+        x = self.conv2(x)
+        x = self.max_pool3d(x)
+        x = self.relu(x)
+        x = self.dropout3d(x)
+        x = self.conv3(x)
+        x = self.max_pool3d(x)
+        x = self.relu(x)
+        x = self.dropout3d(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
-        return F.sigmoid(x, dim=1)
+        x = self.sigmoid(x)
+
+        return x
 
 
+# For testing purposes
 if __name__ == '__main__':
 
     model = Vanilla3DCNN()
