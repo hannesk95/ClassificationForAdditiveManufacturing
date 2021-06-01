179b113c1b1eb38900c912538e1c92fb9e36ed1f
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-01 22:50:16 +0200

    refactor __init__.py

--- src/deep_learning/network/__init__.py
+++ src/deep_learning/network/__init__.py
@@ -1,5 +1,6 @@
 from .ResNet import ResNet
 from .Vanilla3DCNN import Vanilla3DCNN
 from .VGGNet import VGGNet
-from .InceptionNet import InceptionNet_v1, InceptionNet_v3
+from .InceptionNet import InceptionNet_v1
+from .InceptionNet import InceptionNet_v3
 
