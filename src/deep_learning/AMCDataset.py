b45aed456f417d338a1b20f65e5b3a80d7e12bac
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-02 15:21:01 +0200

    update dataset class

--- src/deep_learning/AMCDataset.py
+++ src/deep_learning/AMCDataset.py
@@ -1,16 +1,15 @@
 import os
 import numpy as np
 from torch.utils.data import Dataset
-import torch
 
 
 class AMCDataset(Dataset):
     """Additive Manufacturing Classification (AMC) dataset."""
 
-    def __init__(self, root_dir: str, transform=None, cutoff: int = None):
+    def __init__(self, data_dir: str, transform=None, cutoff: int = None):
         """# TODO: Docstring"""
         super(AMCDataset, self).__init__()
-        self.root_dir = root_dir
+        self.data_dir = data_dir
         self.cutoff = cutoff
         self.models = self._load_model_paths()
         self.transform = transform
@@ -21,11 +20,11 @@ class AMCDataset(Dataset):
 
     def _load_model_paths(self) -> list:
         """# TODO: Docstring"""
-        models = os.listdir(self.root_dir)
+        models = os.listdir(self.data_dir)
         models = [elem for elem in models if elem.endswith('.npz')]
         if self.cutoff is not None:
             models = models[:self.cutoff]
-        models = [os.path.join(self.root_dir, model_path) for model_path in models]
+        models = [os.path.join(self.data_dir, model_path) for model_path in models]
         return models
 
     def __getitem__(self, idx):
@@ -36,5 +35,4 @@ class AMCDataset(Dataset):
         if self.transform:
             model = self.transform(model)
 
-        sample = {'model': torch.from_numpy(model), 'label': torch.from_numpy(label)}
-        return sample
+        return model, label
