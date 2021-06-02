231952093aceb5f4e18804abb156e6787ecfd69a
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-02 15:21:21 +0200

    update architecture selector class

--- src/deep_learning/ArchitectureSelector.py
+++ src/deep_learning/ArchitectureSelector.py
@@ -9,7 +9,7 @@ class ArchitectureSelector:
         """# TODO: Docstring"""
 
         self.nn_architecture = nn_architecture
-        self.architectures = ['Vanilla3DCNN, ResNet, VGGNet, InceptionNet']
+        self.architectures = ["Vanilla3DCNN", "ResNet", "VGGNet", "InceptionNet"]
         self.config = config
         self.model = None
 
@@ -19,24 +19,24 @@ class ArchitectureSelector:
         if self.nn_architecture not in self.architectures:
             raise ValueError(f"Chosen neural network architecture is not valid! Choose out of {self.architectures}")
 
-        if self.nn_architecture is "Vanilla3DCNN":
+        if self.nn_architecture == "Vanilla3DCNN":
             self.model = Vanilla3DCNN()
 
-        if self.nn_architecture is "ResNet":
-            self.model = ResNet.generate_model(self.config.depth_resnet)
+        if self.nn_architecture == "ResNet":
+            self.model = ResNet.generate_model(self.config.resnet_depth)
 
-        if self.nn_architecture is "VGGNet":
+        if self.nn_architecture == "VGGNet":
             self.model = VGGNet()
 
-        if self.nn_architecture is "InceptionNet":
+        if self.nn_architecture == "InceptionNet":
             if self.config.inception_version == 1:
                 self.model = InceptionNet_v1()
             else:
                 self.model = InceptionNet_v3()
 
-        if self.config.optimizer is 'Adam':
+        if self.config.optimizer == 'Adam':
             self.config.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.config.learning_rate)
-        if self.config.optimizer is 'SGD':
+        if self.config.optimizer == 'SGD':
             self.config.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.config.learning_rate)
 
         return self.model
