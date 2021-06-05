54b3ae762682c207ec0ab49c92a8162e6d1450d6
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-05 15:05:38 +0200

    update Vanilla3DCNN class

--- src/deep_learning/network/Vanilla3DCNN.py
+++ src/deep_learning/network/Vanilla3DCNN.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torchsummary import summary
+# import torch.nn.functional as F
 
 
 class Vanilla3DCNN(nn.Module):
@@ -11,38 +12,39 @@ class Vanilla3DCNN(nn.Module):
 
         super(Vanilla3DCNN, self).__init__()
 
-        self.conv1 = nn.Conv3d(in_channels=1, out_channels=32, kernel_size=(9, 9, 9))
-        self.conv2 = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=(7, 7, 7))
-        self.conv3 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(5, 5, 5))
-        self.fc1 = nn.Linear(46656, 64)
+        self.conv1 = nn.Conv3d(in_channels=1, out_channels=1, kernel_size=(3, 3, 3))
+        self.conv2 = nn.Conv3d(in_channels=1, out_channels=1, kernel_size=(3, 3, 3))
+        # self.conv3 = nn.Conv3d(in_channels=32, out_channels=64, kernel_size=(3, 3, 3))
+        self.fc1 = nn.Linear(24389, 64)
         self.fc2 = nn.Linear(64, 1)
 
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.2)
-        self.dropout3d = nn.Dropout3d(p=0.2)
+        # self.relu = nn.ReLU()
+        # self.dropout = nn.Dropout(p=0.2)
+        # self.dropout3d = nn.Dropout3d(p=0.2)
         self.max_pool3d = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=2)
-        self.sigmoid = nn.Sigmoid()
+        # self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
         """#TODO: Add docstring."""
 
-        x = self.conv1(x)
-        x = self.relu(x)
-        x = self.dropout3d(x)
-        x = self.conv2(x)
+        x = torch.relu(self.conv1(x))
         x = self.max_pool3d(x)
-        x = self.relu(x)
-        x = self.dropout3d(x)
-        x = self.conv3(x)
+        # x = self.relu(x)
+        # x = self.dropout3d(x)
+        x = torch.relu(self.conv2(x))
         x = self.max_pool3d(x)
-        x = self.relu(x)
-        x = self.dropout3d(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = self.dropout(x)
-        x = self.fc2(x)
-        x = self.sigmoid(x)
+        # x = self.relu(x)
+        # x = self.dropout3d(x)
+        # x = torch.relu(self.conv3(x))
+        # x = self.max_pool3d(x)
+        # x = self.relu(x)
+        # x = self.dropout3d(x)
+        # x = x.view(x.size(0), -1)
+        x = torch.flatten(x)
+        x = torch.relu(self.fc1(x))
+        # x = self.relu(x)
+        # x = self.dropout(x)
+        x = torch.sigmoid(self.fc2(x))
 
         return x
 
@@ -53,6 +55,6 @@ if __name__ == '__main__':
     model = Vanilla3DCNN()
 
     if torch.cuda.device_count() > 0:
-        summary(model.cuda(), (1, 64, 64, 64))
+        summary(model.cuda(), (1, 128, 128, 128))
     else:
-        summary(model, (1, 64, 64, 64))
+        summary(model, (1, 128, 128, 128))
