a84c5f3f403bc1cae96f8f3ce2838e753c53411f
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-05 18:45:28 +0200

    update main_deep_learning.py

--- src/deep_learning/main_deep_learning.py
+++ src/deep_learning/main_deep_learning.py
@@ -1,7 +1,5 @@
 import sys
-
 import torch.utils.data
-
 sys.path.append(".")   #TODO Ugly - currently needed for LRZ AI System - find better solution
 sys.path.append("..")
 sys.path.append("../..")
