e48e994f7d6cda2da11a46f513c765f9c0e421ea
ge46tik <aditya.srinivas@tum.de>
2021-06-06 12:16:26 +0200

    Fixed bugs in Inception_v3

--- src/deep_learning/network/InceptionNet.py
+++ src/deep_learning/network/InceptionNet.py
@@ -88,8 +88,8 @@ class InceptionNet_v3(nn.Module):
         self.inception5 = Inception_Block_v3(in_channels=512,out_1x1x1=128,red1_3x3x3=256, red2_3x3x3=256, out_3x3x3=384, red_3x3x3=256, out2_3x3x3=384, out_1x1x1_pool=128)
         self.inception6 = Inception_Block_v3(in_channels=1024,out_1x1x1=128,red1_3x3x3=256, red2_3x3x3=256, out_3x3x3=384, red_3x3x3=256, out2_3x3x3=384, out_1x1x1_pool=128)
         
-        self.avgpool = nn.AvgPool3d(kernel_size=2,stride=1)
-
+        self.avgpool = nn.AvgPool3d(kernel_size=4,stride=1)
+        
         self.dropout = nn.Dropout3d(p=0.4)
 
         self.fc1 = nn.Linear(1024,512)
@@ -105,7 +105,6 @@ class InceptionNet_v3(nn.Module):
        x = self.conv2(x)
        x = self.maxpool(x)
        
-       
        x = self.inception3(x)
        x = self.maxpool(x)
        x = self.inception4(x)
@@ -118,7 +117,7 @@ class InceptionNet_v3(nn.Module):
        x = x.view(x.size(0),-1)
        
        x = self.dropout(x)
-
+        
        x = self.fc1(x)
        x = self.relu(self.fc2(x))
        x = self.sigmoid(self.fc3(x))
@@ -136,7 +135,7 @@ class Inception_Block_v3(nn.Module):
         self.branch_4 = nn.Sequential(nn.MaxPool3d(kernel_size=3,stride=1,padding=1),conv_block(in_channels,out_1x1x1_pool,kernel_size=1))
 
     def forward(self,x):
-        return torch.cat([self.branch_1(x),self.branch_2(x),self.branch_3(x),self.branch_4(x)])
+        return torch.cat([self.branch_1(x),self.branch_2(x),self.branch_3(x),self.branch_4(x)],1)
 
 
 class conv_block(nn.Module):
@@ -149,13 +148,11 @@ class conv_block(nn.Module):
     def forward(self,x):
         return self.relu(self.batchnorm(self.conv(x)))
 
-"""
 #Testing only
 if __name__ == '__main__':
-    net = InceptionNet_v1()
-    
+    net = InceptionNet_v3()
     if torch.cuda.device_count() > 0:
-        summary(net.cuda(),(1,64,64,64))
+        summary(net.cuda(),(1,128,128,128))
     else:
-        summary(net,(1,64,64,64))
-"""
+        summary(net,(1,128,128,128))
+    
