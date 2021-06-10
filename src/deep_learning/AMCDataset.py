8ac40c21dce1925a12eb563294c87ce212e13a58
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-10 18:25:47 +0200

    clean up AMCDataset class

--- src/deep_learning/AMCDataset.py
+++ src/deep_learning/AMCDataset.py
@@ -31,20 +31,12 @@ class AMCDataset(Dataset):
     def __getitem__(self, idx):
         """# TODO: Docstring"""
         model = np.load(self.models[idx])['model']
-        # model = np.reshape(model, (1, model.shape[0], model.shape[1], model.shape[2]))
-        # model = model.astype(np.float32)
-
         label = torch.tensor(np.load(self.models[idx])['label'], dtype=torch.float32)
-        # label = torch.FloatTensor(label).unsqueeze(-1)
-        # label = np.reshape(label, (-1, 1))
-        # label = label.astype(np.float32)
 
         if self.transform:
             model = self.transform(model)
-            # label = self.transform(label)
 
         model = torch.reshape(model, (1, model.shape[0], model.shape[1], model.shape[2]))
         model = model.to(torch.float32)
-        # label = label.to(torch.float32)
 
         return model, label
