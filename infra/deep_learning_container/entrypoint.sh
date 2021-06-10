6b7ef0074ae5ad228a701e00fc7d15ada51a70f4
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-10 21:29:29 +0200

    fix: entrypoint

--- infra/deep_learning_container/entrypoint.sh
+++ infra/deep_learning_container/entrypoint.sh
@@ -2,9 +2,9 @@
 
 # Clone current status of project repository
 git clone https://hannesk95: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@github.com/semester-project/TUM-DI-LAB.git
-cd TUM-DI-LAB
+cd /workspace/TUM-DI-LAB
 git checkout dev
-cd /
+cd /workspace
 
 # Start bash
 /bin/bash
