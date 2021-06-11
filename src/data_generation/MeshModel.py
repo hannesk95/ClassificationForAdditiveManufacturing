bc1733cceb0a5938e592bbd61c1950e1f9be7e4c
fxbk <44337498+fxbk@users.noreply.github.com>
2021-06-11 10:11:18 +0200

    Refactor: Moved visulaitation functions in the respective data class and in utils, deleted visualization and test_visualization

--- src/data_generation/MeshModel.py
+++ src/data_generation/MeshModel.py
@@ -1,6 +1,9 @@
 import numpy as np
+import os
+from stl import mesh
 import open3d as o3d
-from src.data_generation.utils import extract_file_name
+import plotly.graph_objects as go
+from src.data_generation.utils import extract_file_name, get_layout
 
 
 def _convert_to_o3d_mesh(vertices, faces):
@@ -69,6 +72,51 @@ class MeshModel:
         """
 
         o3d.visualization.draw_geometries([self.mesh] + geometries)
+
+    def plot_mesh(self, target_name=None, save=True):
+        """
+        Creates a 3D interactive JavaScript plot of a given stl 3D model
+
+        :param stl_path: Source path to the stl model to plot
+        :param target_name: name of the resulting html file. If not provided the name of the given file in stl_path will be
+        used
+        :param save: boolean: Deciding wheter the output model should be saved or not
+        :return: Html file containing the plot
+        """
+        my_mesh = mesh.Mesh.from_file(self.path)
+
+        p, q, r = my_mesh.vectors.shape  # (p, 3, 3)
+        # the array stl_mesh.vectors.reshape(p*q, r) can contain multiple copies of the same vertex;
+        # extract unique vertices from all mesh triangles
+        vertices, ixr = np.unique(my_mesh.vectors.reshape(p * q, r), return_inverse=True, axis=0)
+        I = np.take(ixr, [3 * k for k in range(p)])
+        J = np.take(ixr, [3 * k + 1 for k in range(p)])
+        K = np.take(ixr, [3 * k + 2 for k in range(p)])
+
+        x, y, z = vertices.T
+
+        mesh3D = go.Mesh3d(
+            x=x,
+            y=y,
+            z=z,
+            i=I,
+            j=J,
+            k=K,
+            flatshading=True,
+            colorscale=self.colorscale,
+            intensity=z,
+            name=target_name,
+            showscale=False)
+
+        title = f"STL Model {target_name}"
+        layout = get_layout(title)
+        fig = go.Figure(data=[mesh3D], layout=layout)
+
+        if save:
+            target_path = os.path.join(self.target_dir, target_name + '.html')
+            fig.write_html(target_path)
+        else:
+            fig.show()
 
     def mesh_checks(self):
         """
