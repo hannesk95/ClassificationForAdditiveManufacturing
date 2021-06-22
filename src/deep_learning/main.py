52ad37afeae4e55d5babb46fa56e33053f55b8a1
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-22 12:27:45 +0200

    update main.py

--- src/deep_learning/main.py
+++ src/deep_learning/main.py
@@ -15,7 +15,7 @@ from torch.utils.data import DataLoader, random_split
 import torch.nn.functional as F
 import torchmetrics
 
-import horovod.torch as hvd
+# import horovod.torch as hvd
 
 from src.deep_learning.AMCDataset import AMCDataset
 from src.deep_learning.ParamConfigurator import ParamConfigurator
@@ -23,43 +23,44 @@ from src.deep_learning.ClassificationTask import ClassificationTask
 from src.deep_learning.ArchitectureSelector import ArchitectureSelector
 from src.deep_learning.FailureAnalyst import FailureAnalyst
 
-def metric_average(val, name):
-    tensor = val.detach().clone()
-    avg_tensor = hvd.allreduce(tensor, name=name)
-    return avg_tensor.item()
+# def metric_average(val, name):
+#     tensor = val.detach().clone()
+#     avg_tensor = hvd.allreduce(tensor, name=name)
+#     return avg_tensor.item()
 
-def test(model, test_sampler, test_loader):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = model.to(device)
+# def test(model, test_sampler, test_loader):
+#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#     model = model.to(device)
+#
+#     model.eval()
+#     test_loss = 0.
+#     test_accuracy = 0.
+#
+#     for data, target in test_loader:
+#         data, target = data.to(device), target.to(device)
+#         output = model(data).clone().detach().cpu()
+#         # sum up batch loss
+#         test_loss += F.binary_cross_entropy_with_logits(output, target.clone().detach().cpu())
+#         test_accuracy += torchmetrics.Accuracy()(output.round().int(), target.clone().detach().cpu().int())
+#
+#     # Horovod: use test_sampler to determine the number of examples in
+#     # this worker's partition.
+#     test_loss /= len(test_sampler)
+#     test_accuracy /= len(test_sampler)
+#
+#     # Horovod: average metric values across workers.
+#     test_loss = metric_average(test_loss, 'avg_loss')
+#     test_accuracy = metric_average(test_accuracy, 'avg_accuracy')
+#
+#     # Horovod: print output only on first rank.
+#     if hvd.rank() == 0:
+#         print('\nTest set: Average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(
+#             test_loss, 100. * test_accuracy))
 
-    model.eval()
-    test_loss = 0.
-    test_accuracy = 0.
-
-    for data, target in test_loader:
-        data, target = data.to(device), target.to(device)
-        output = model(data).clone().detach().cpu()
-        # sum up batch loss
-        test_loss += F.binary_cross_entropy_with_logits(output, target.clone().detach().cpu())
-        test_accuracy += torchmetrics.Accuracy()(output.round().int(), target.clone().detach().cpu().int())
-
-    # Horovod: use test_sampler to determine the number of examples in
-    # this worker's partition.
-    test_loss /= len(test_sampler)
-    test_accuracy /= len(test_sampler)
-
-    # Horovod: average metric values across workers.
-    test_loss = metric_average(test_loss, 'avg_loss')
-    test_accuracy = metric_average(test_accuracy, 'avg_accuracy')
-
-    # Horovod: print output only on first rank.
-    if hvd.rank() == 0:
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {:.2f}%\n'.format(
-            test_loss, 100. * test_accuracy))
 
 def main():
 
-    hvd.init()
+    # hvd.init()
 
     # 1. Define configuration parameters
     config = ParamConfigurator()
@@ -84,10 +85,10 @@ def main():
     train_data_loader = DataLoader(train_data, batch_size=config.batch_size, shuffle=True, **config.kwargs)
     validation_data_loader = DataLoader(val_data, batch_size=config.batch_size, shuffle=False, **config.kwargs)
 
-    test_sampler = torch.utils.data.distributed.DistributedSampler(
-        val_data, num_replicas=hvd.size(), rank=hvd.rank())
-    test_loader = torch.utils.data.DataLoader(val_data, batch_size=config.batch_size, shuffle=False,
-                                              sampler=test_sampler, **config.kwargs)
+    # test_sampler = torch.utils.data.distributed.DistributedSampler(
+    #     val_data, num_replicas=hvd.size(), rank=hvd.rank())
+    # test_loader = torch.utils.data.DataLoader(val_data, batch_size=config.batch_size, shuffle=False,
+    #                                           sampler=test_sampler, **config.kwargs)
 
     # 7. Start MLflow logging
     mlflow.set_tracking_uri(config.mlflow_log_dir)
@@ -99,14 +100,15 @@ def main():
 
     # 9. Start training
     trainer = pl.Trainer(max_epochs=config.num_epochs, deterministic=True, accelerator='horovod', gpus=1, precision=16)
+    # trainer = pl.Trainer(max_epochs=config.num_epochs, deterministic=True)
     trainer.fit(classifier, train_data_loader, validation_data_loader)
 
     # 10. Perform failure analysis
-    # analyst = FailureAnalyst(config=config, trainer=trainer, val_data=val_data)
-    # analyst.start_failure_analysis()
+    analyst = FailureAnalyst(config=config, val_data=val_data, nn_model=nn_model)
+    analyst.start_failure_analysis()
 
     # 11. Testing
-    test(nn_model, test_sampler, test_loader)
+    # test(nn_model, test_sampler, test_loader)
 
 
 if __name__ == '__main__':
