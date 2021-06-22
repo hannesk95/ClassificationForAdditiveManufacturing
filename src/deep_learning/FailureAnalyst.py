a882a43d38c0a8c679e342ab345ee2c54069527b
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-22 17:45:41 +0200

    fix: batch prediction

--- src/deep_learning/FailureAnalyst.py
+++ src/deep_learning/FailureAnalyst.py
@@ -1,6 +1,8 @@
 import numpy as np
 import mlflow
 import torch
+import sys
+import os
 
 
 class FailureAnalyst:
@@ -17,13 +19,17 @@ class FailureAnalyst:
 
         # Get true labels & predicted labels
         true_labels = []
-        pred_labels = []
+        val_models = []
         for i in range(len(self.val_data)):
             true_labels.append(self.val_data[i][1])
-            pred_labels.append(torch.round(self.nn_model(torch.unsqueeze(self.val_data[i][0], 0))))
+            val_models.append(self.val_data[i][0].numpy())
+
+        pred_labels = torch.round(self.nn_model(torch.Tensor(val_models)))
+
+        # pred_labels.append(torch.round(self.nn_model(torch.unsqueeze(self.val_data[i][0], 0))))
 
         # Compare true labels and predicted labels
-        result = np.equal(np.array(true_labels, dtype=int), np.array(pred_labels, dtype=int))
+        result = np.equal(np.array(true_labels, dtype=int), pred_labels.detach().numpy().flatten())
 
         # Get indices of failed predictions and store respective model path
         failure_idx = list(np.where(result == False)[0])
@@ -31,5 +37,13 @@ class FailureAnalyst:
         for i in failure_idx:
             failed_models.append(self.val_data.dataset.models[i])
 
-        # Store list of failed models using MLflow
-        mlflow.log_artifact("list_failed_models", failed_models)
+        # Store paths/names of failed models using MLflow
+        orig_stdout = sys.stdout
+        f = open('failed_models.txt', 'w')
+        sys.stdout = f
+        for i in range(len(failed_models)):
+            print(failed_models[i])
+        sys.stdout = orig_stdout
+        f.close()
+        mlflow.log_artifact("failed_models.txt")
+        os.remove("failed_models.txt")
