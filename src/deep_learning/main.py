2cf2c89c9a11a1032dee2aa187adc52bece71a71
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-23 09:13:14 +0200

    fix: pass validation dataloader

--- src/deep_learning/main.py
+++ src/deep_learning/main.py
@@ -104,7 +104,8 @@ def main():
     trainer.fit(classifier, train_data_loader, validation_data_loader)
 
     # 10. Perform failure analysis
-    analyst = FailureAnalyst(config=config, val_data=val_data, nn_model=nn_model, trainer=trainer)
+    analyst = FailureAnalyst(config=config, val_data=val_data, nn_model=nn_model, trainer=trainer,
+                             val_dataloader=validation_data_loader)
     analyst.start_failure_analysis()
 
     # 11. Testing
