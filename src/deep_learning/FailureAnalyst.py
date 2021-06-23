4fff5c250bcc110d74fedc0a83f0ff0450ebc5a6
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-06-23 08:42:44 +0200

    feat: print progress of failure analysis

--- src/deep_learning/FailureAnalyst.py
+++ src/deep_learning/FailureAnalyst.py
@@ -13,36 +13,35 @@ class FailureAnalyst:
         """# TODO: Docstring"""
         self.config = config
         self.val_data = val_data
-        self.nn_model = nn_model
+        self.nn_model = nn_model.eval()
 
     def start_failure_analysis(self):
         """# TODO: Docstring"""
 
-        torch.cuda.empty_cache()
         logging.info('Start failure analysis')
-
-
 
         # Get true labels & predicted labels
         true_labels = []
         val_models = []
-        for i in tqdm(range(len(self.val_data)), desc="Getting inference data"):
+        pred_labels = []
+        for i in tqdm(range(len(self.val_data)), desc="Performing failure analysis"):
             true_labels.append(self.val_data[i][1])
-            val_models.append(self.val_data[i][0])
+            # val_models.append(self.val_data[i][0])
+            pred_labels.append(torch.round(self.nn_model(torch.unsqueeze(self.val_data[i][0], 0))))
 
-        models = torch.stack(val_models, dim=0)
-        models = models
-        self.nn_model = self.nn_model.eval()
+        # models = torch.stack(val_models, dim=0)
+        # models = models
 
-        logging.info("Start prediction")
-        pred_labels = torch.round(self.nn_model(models))
-        logging.info("End prediction")
+        # logging.info("Start prediction")
+        # pred_labels = torch.round(self.nn_model(models))
+        # logging.info("End prediction")
 
 
         # pred_labels.append(torch.round(self.nn_model(torch.unsqueeze(self.val_data[i][0], 0))))
 
         # Compare true labels and predicted labels
-        result = np.equal(np.array(true_labels, dtype=int), pred_labels.detach().numpy().flatten())
+        # result = np.equal(np.array(true_labels, dtype=int), pred_labels.detach().numpy().flatten())
+        result = np.equal(np.array(true_labels, dtype=int), np.array(pred_labels, dtype=int))
 
         # Get indices of failed predictions and store respective model path
         failure_idx = list(np.where(result == False)[0])
