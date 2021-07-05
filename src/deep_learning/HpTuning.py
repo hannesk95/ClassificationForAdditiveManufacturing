acdbec06c9cdb578b521c27029ce2bd36e41025d
Nouhayla Bouziane <38696443+nouhayla1234@users.noreply.github.com>
2021-07-05 13:04:24 +0200

    Update HpTuning.py

--- src/deep_learning/HpTuning.py
+++ src/deep_learning/HpTuning.py
@@ -1,3 +1,12 @@
+
+
+import sys
+
+sys.path.append(".")   #TODO Ugly - currently needed for LRZ AI System - find better solution
+sys.path.append("..")
+sys.path.append("../..")
+
+
 import optuna
 import mlflow
 import torch
