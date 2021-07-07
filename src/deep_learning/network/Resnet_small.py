1337a7e7a209ea4d1f6b53156f0a42c7aeceedde
ge46tik <aditya.srinivas@tum.de>
2021-07-07 15:27:33 +0200

    architecture change

--- src/deep_learning/network/Resnet_small.py
+++ src/deep_learning/network/Resnet_small.py
@@ -7,7 +7,7 @@ from torchsummary import summary
 
 
 def get_inplanes():
-    return [64,128,256,512]
+    return [64,128,256]
 
 
 def conv3x3x3(in_planes, out_planes, stride=1):
@@ -60,7 +60,7 @@ class BasicBlock(nn.Module):
 
 class Resnet_small(nn.Module):
 
-    def __init__(self,block,layer,block_inplanes,n_input_channels=1,conv1_t_size=7,conv1_t_stride=1,no_max_pool=False,shortcut_type='B',widen_factor=1.0,n_classes=256):
+    def __init__(self,block,layer,block_inplanes,n_input_channels=1,conv1_t_size=7,conv1_t_stride=1,no_max_pool=False,shortcut_type='B',widen_factor=1.0,n_classes=512):
         super().__init__()
 
         block_inplanes = [int(x * widen_factor) for x in block_inplanes]
@@ -75,15 +75,15 @@ class Resnet_small(nn.Module):
         self.layer1 = self._make_layer(block, block_inplanes[0], layer[0],shortcut_type)
         self.layer2 = self._make_layer(block,block_inplanes[1],layer[1],shortcut_type,stride=2)
         self.layer3 = self._make_layer(block,block_inplanes[2],layer[2],shortcut_type,stride=2)
-        self.layer4 = self._make_layer(block,block_inplanes[3],layer[3],shortcut_type,stride=2)
-
+        #self.layer4 = self._make_layer(block,block_inplanes[3],layer[3],shortcut_type,stride=2)
+        self.maxpool1 = nn.MaxPool3d(kernel_size=5, stride=1)
         self.avgpool = nn.AvgPool3d(kernel_size=4,stride=1)
-        self.fc1 = nn.Linear(512, n_classes)
+        self.fc1 = nn.Linear(1024, n_classes)
         self.fc2 = nn.Linear(n_classes, 128)
         self.fc3 = nn.Linear(128, 64)
         self.fc4 = nn.Linear(64, 1)
         self.sigmoid = nn.Sigmoid()
-        
+        self.convf1 = conv1x1x1(256,1024)
         
         for m in self.modules():
             if isinstance(m, nn.Conv3d):
@@ -135,13 +135,14 @@ class Resnet_small(nn.Module):
         #print(x.shape)
         x = self.layer3(x)
         
-        x = self.layer4(x)
-       
+        #x = self.layer4(x)
+        x = self.maxpool1(x)
         x = self.avgpool(x)
+        x = self.convf1(x)
         
         x = x.view(x.size(0), -1)
         
-        #print(x.shape)
+       
         x = self.fc1(x)
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
@@ -152,7 +153,7 @@ class Resnet_small(nn.Module):
 
 
 def generate_model(**kwargs):
-    model = Resnet_small(BasicBlock, [1,1,1,1], get_inplanes(), **kwargs)
+    model = Resnet_small(BasicBlock, [2,2,2], get_inplanes(), **kwargs)
     return model
 
 
