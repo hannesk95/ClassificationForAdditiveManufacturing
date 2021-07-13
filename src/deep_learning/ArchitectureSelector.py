1cca586a3d0fce47f78ecdc0a53b78e6caceb1d2
Johannes Kiechle <hannes.kiechle@gmail.com>
2021-07-13 08:51:57 +0200

    update architecture selector

--- src/deep_learning/ArchitectureSelector.py
+++ src/deep_learning/ArchitectureSelector.py
@@ -9,38 +9,38 @@ class ArchitectureSelector:
         """# TODO: Docstring"""
 
         self.config = config
-        self.nn_architecture = self.config.architecture_type
+        # self.nn_architecture = self.config.architecture_type
         self.architectures = ["Vanilla3DCNN", "ResNet", "VGGNet", "InceptionNet", "Vanilla3DCNN_large", "Resnet_small"]
         self.model = None
 
     def select_architecture(self):
         """# TODO: Docstring"""
 
-        if self.nn_architecture not in self.architectures:
+        if self.config.architecture_type not in self.architectures:
             raise ValueError(f"Chosen neural network architecture is not valid! Choose out of {self.architectures}")
 
-        if self.nn_architecture == "Vanilla3DCNN":
+        if self.config.architecture_type == "Vanilla3DCNN":
             self.model = Vanilla3DCNN()
 
-        if self.nn_architecture == "Vanilla3DCNN_large":
+        if self.config.architecture_type == "Vanilla3DCNN_large":
             self.model = Vanilla3DCNN_large()
 
-        if self.nn_architecture == "ResNet":
+        if self.config.architecture_type == "ResNet":
             self.model = ResNet.generate_model(self.config.resnet_depth, self.config.resnet_pretrained)
-            self.config.experiment_name = self.nn_architecture + str(self.config.resnet_depth) + "_pretrained_" + str(self.config.resnet_pretrained)
+            self.config.experiment_name = self.config.architecture_type + str(self.config.resnet_depth) + "_pretrained_" + str(self.config.resnet_pretrained)
         
-        if self.nn_architecture == "Resnet_small":
+        if self.config.architecture_type == "Resnet_small":
             self.model = Resnet_small.generate_model()
 
-        if self.nn_architecture == "VGGNet":
+        if self.config.architecture_type == "VGGNet":
             self.model = VGGNet()
 
-        if self.nn_architecture == "InceptionNet":
+        if self.config.architecture_type == "InceptionNet":
             if self.config.inception_version == 1:
                 self.model = InceptionNet_v1()
             else:
                 self.model = InceptionNet_v3()
-            self.config.experiment_name = self.nn_architecture + str(self.config.inception_version)
+            self.config.experiment_name = self.config.architecture_type + str(self.config.inception_version)
 
         # if self.config.device.type == 'cuda':
 
